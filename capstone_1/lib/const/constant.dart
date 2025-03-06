1679da096f339b4b9f9c538aec3a79efdc94270e
koren <nowtoknow@gmail.com>
2025-03-07 01:21:52 +0800

    structure_complete_need_layout

--- capstone_1/lib/const/constant.dart
+++ capstone_1/lib/const/constant.dart
@@ -4,7 +4,7 @@ const cardBgColor = Color.fromARGB(0, 0, 0, 0);
 const primaryColor = Color.fromARGB(207, 43, 196, 170);
 const secondaryColor = Color.fromARGB(186, 81, 27, 153);
 const backgroundColor = Color.fromARGB(166, 189, 23, 23);
-const selectionColor = Color.fromARGB(157, 85, 151, 10);
+const selectionColor = Color.fromARGB(157, 174, 209, 218);
 
 const defaultPadding = 20;
 
--- capstone_1/lib/widgets/side_menu_widget.dart
+++ capstone_1/lib/widgets/side_menu_widget.dart
@@ -1,4 +1,6 @@
+import 'package:capstone_1/const/constant.dart';
 import 'package:flutter/material.dart';
+import 'package:capstone_1/data/side_menu_data.dart';
 
 class SideMenuWidget extends StatefulWidget{
 
@@ -10,9 +12,59 @@ class SideMenuWidget extends StatefulWidget{
 }
 
 class _SideMenuGetState extends State<SideMenuWidget> {
+  int selectedIndex = 0;
 
   @override
   Widget build(BuildContext context){
-    return Container();
+    final data = SideMenuData();
+
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal:20,vertical:70),
+      child: ListView.builder(
+        itemCount: data.menu.length,
+        itemBuilder: (context,index) => buildMenuEntry(data,index),
+        ), 
+    );
+  }
+
+  Widget buildMenuEntry(SideMenuData data, int index){
+    final isSelected = index == selectedIndex;
+
+      return Container(
+        
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(6.0)),
+        
+          color: isSelected ? selectionColor: Colors.transparent,
+        ),
+        child:InkWell(
+          onTap: () => setState(() {
+              selectedIndex = index;
+            }),
+          hoverColor: const Color.fromARGB(255, 25, 125, 206).withAlpha(25), // Add hover color here
+        
+          child:Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal:10,vertical:20),
+                ),
+
+              Icon(
+                data.menu[index].icon,
+                color: isSelected ? Colors.black : Colors.grey,
+                ),
+
+              Text(
+                data.menu[index].title,
+                style: TextStyle(
+                  color: isSelected ? Colors.black : Colors.grey,
+                  fontSize: 16.0,
+                  fontWeight: isSelected ? FontWeight.bold : FontWeight.normal
+                ),
+                ),
+            ],
+          ),
+        ),
+      );
   }
 }
